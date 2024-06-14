6e5e2a8f0ce09989abf71d9cc4fcdba00fdcf688
Sachit <sachitreddy.7@gmail.com>
2024-06-14 22:59:42 +0530

    Multiline Comments

---
+++ Multiline Comments.py
@@ -0,0 +1,14 @@
+print(1)
+print(2)
+print(3)
+# ctrl + /
+
+'''This Program takes an  integer input from the user and prints it'''
+number = int(input("Enter an integer"))
+print("You entered:", number)
+
+"""This program takes name and age from the user and prints it"""
+name = input("Enter Your Name")
+age = int(input("Enter Your Age"))
+print("Your Name is:",    name)
+print("Your Age Is: ", age)
